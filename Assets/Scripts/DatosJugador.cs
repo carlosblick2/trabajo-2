5ed7d34d5b05e8a3c41b81cca89b4e030eedaf79
carlosblick2 <135982868+carlosblick2@users.noreply.github.com>
2023-10-26 00:56:19 -0300

    8
    menu inicial y vida del player

---
+++ Assets/Scripts/DatosJugador.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DatosJugador : MonoBehaviour
+{
+    public int vidaPlayer;
+    public Slider vidaVisual;
+        private void Update()
+    {
+
+        vidaVisual.GetComponent<Slider>().value = vidaPlayer;
+
+        if(vidaPlayer <=0)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+
+}
